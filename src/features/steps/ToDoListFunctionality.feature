2153532d885e2e685aa1dcab85e2b8eeced869c3
Oliver <info@oliver-fries.de>
2024-01-29 23:07:12 +0100

    BDD update

--- src/features/steps/ToDoListFunctionality.feature
+++ src/features/steps/ToDoListFunctionality.feature
@@ -11,4 +11,22 @@ Feature: To Do List functionality
     When I mark the task as done
     Then the ToDo list should contain a task "Read a book" marked as done
 
-    # Try to enter an empty task, Remove a task from the list and should be shown in archive
+  Scenario: Remove a task from the ToDo list
+    Given the ToDo list contains a task "Write report" marked as not done
+    When I remove the task "Write report"
+    Then the ToDo list should not contain a task "Write report"
+
+  Scenario: Archive a completed task
+    Given the ToDo list contains a task "Exercise" marked as done
+    When I remove the task "Exercise"
+    Then the ToDo list should not contain a task "Exercise"
+    And the Archive should contain a task "Exercise" marked as done
+
+  Scenario: Load tasks from a saved state
+    Given the ToDo list is empty
+    When I load tasks from a saved state with tasks:
+        | title           | description       | done |
+        | Study Python    | Practice coding   | false|
+        | Learn React      | Build a web app   | true |
+    Then the ToDo list should contain a task "Study Python" marked as not done
+    And the ToDo list should contain a task "Learn React" marked as done
--- src/features/steps/steps/ToDoListFunctionality.py
+++ src/features/steps/steps/ToDoListFunctionality.py
@@ -1,32 +1,93 @@
-from behave import given, when, then, register_type
+import lettuce
+from behave import given, when, then
+from src.todolist.ToDoList import ToDoList
+from src.todolist.ArchiveManager import ArchiveManager
+from src.todolist.TaskManager import TaskManager
+
 
 @given('the ToDo list is empty')
 def step_given_todo_list_empty(context):
-    context.todo_list = []
-
-
-@when('I add a task with title "{title}" and description "{description}"')
-def step_when_add_task(context, title, description):
-    task = {'title': title, 'description': description, 'done': False}
-    context.todo_list.append(task)
-
-
-@then('the ToDo list should contain "{title}"')
-def step_then_todo_list_contains(context, title):
-    assert any(task['title'] == title for task in context.todo_list), f'Task "{title}" not found'
+    context.task_manager = TaskManager()
 
 
 @given('the ToDo list contains a task "{title}"')
 def step_given_todo_list_contains(context, title):
-    context.todo_list = [{'title': title, 'done': False}]
+    context.task_manager = TaskManager()
+    context.task_manager.add_task(title)
+    tasks = context.task_manager.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+
+
+@when('I add a task with title "{title}" and description "{description}"')
+def step_when_add_task(context, title, description):
+    context.task_manager.add_task(title, description)
+
+
+@then('the ToDo list should contain "{title}"')
+def step_then_todo_list_contains(context, title):
+    tasks = context.task_manager.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+
+
+@given('the ToDo list contains a task "{title}" marked as not done')
+def step_given_todo_list_contains_not_done(context, title):
+    context.task_manager.add_task(title)
+    tasks = context.task_manager.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+    assert 'Done' not in tasks, f'Task "{title}" should not be marked as done'
+
+
+@given('the ToDo list contains a task "{title}" marked as done')
+def step_given_todo_list_contains_done(context, title):
+    context.task_manager.add_task(title)
+    context.task_manager.mark_as_done(0)
+    tasks = context.task_manager.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+    assert 'Done' in tasks, f'Task "{title}" should be marked as done'
 
 
 @when('I mark the task as done')
 def step_when_mark_task_done(context):
-    context.todo_list[0]['done'] = True
+    context.task_manager.mark_as_done(0)
 
 
 @then('the ToDo list should contain a task "{title}" marked as done')
 def step_then_todo_list_contains_done(context, title):
-    matching_tasks = [task for task in context.todo_list if task['title'] == title and task['done']]
-    assert matching_tasks, f'No task with title "{title}" marked as done found'
+    tasks = context.todo_list.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+    assert f'✔ {title}' in tasks, f'Task "{title}" should be marked as done'
+
+
+@when('I remove the task "{title}"')
+def step_when_remove_task(context, title):
+    context.task_manager.remove_task(0)
+
+
+@then('the ToDo list should not contain a task "{title}"')
+def step_then_todo_list_not_contains(context, title):
+    tasks = context.task_manager.show_tasks()
+    assert title not in tasks, f'Task "{title}" still found in ToDo list'
+
+
+@when('I load tasks from a saved state with tasks:')
+def step_when_load_tasks_from_saved_state(context):
+    tasks_table = context.table
+    context.todo_list = ToDoList()
+    for row in tasks_table.rows:
+        context.todo_list.task_manager.add_task(row['title'], row['description'])
+        if row['done'].lower() == 'true':
+            context.todo_list.task_manager.mark_as_done(0)
+
+
+@then('the Archive should contain a task "{title}" marked as done')
+def step_then_archive_contains_done(context, title):
+    archive = context.task_manager.show_archive()
+    assert title in archive, f'Task "{title}" not found in Archive'
+    assert 'Done' in archive, f'Task "{title}" should be marked as done in Archive'
+
+
+@then('the ToDo list should contain a task "{title}" marked as not done')
+def step_then_todo_list_contains_not_done(context, title):
+    tasks = context.task_manager.show_tasks()
+    assert title in tasks, f'Task "{title}" not found in ToDo list'
+    assert 'Done' not in tasks, f'Task "{title}" should not be marked as done in ToDo list'
